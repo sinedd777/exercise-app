eb877adc2e2e22c54013bec51121d37ffb5c7ff4
sinedd777 <gaurang.work20@gmail.com>
2022-07-14 04:59:16 +0530

    Navbar and banner done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,32 @@ import Logo from '../assets/images/Logo.png'
 
 const Navbar = () => {
   return (
-    <Stack>
+    <Stack 
+        direction="row"
+        sx={{ gap: {sm:'122px', xs:'40px'}, mt: {sm:'32px', xs:'20px'}, justifyContent:'none'}} 
+        justifyContent="space-around" 
+    >
         <Link to="/">
             <img src = {Logo} alt="logo" style={{
                 width: '48px', height: '48px', margin: '0 20px'
             }}></img>
         </Link>
-        <Stack>
-            <Link to="/" style={{ textDecoration: 'none'}}>Home</Link>
-            <a href='#exercises' style={{
-                textDecoration: 'none'
-            }}>Exercises</a>
+        <Stack
+            direction="row"
+            gap="40px"
+            fontSize="24px"
+            alignItems="flex-end" 
+        >
+            <Link 
+                to="/" 
+                style={{ textDecoration: 'none'}}>
+                    Home
+            </Link>
+            <a 
+            href='#exercises' 
+            style={{ textDecoration: 'none'}}>
+                Exercises
+            </a>
         </Stack>
     </Stack>
     )
