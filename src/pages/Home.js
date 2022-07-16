62ca74fad1b8fc4b588fbb3c3a88a50c3f5b0281
sinedd777 <gaurang.work20@gmail.com>
2022-07-16 11:45:13 +0530

    Working home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,9 @@ const Home = () => {
   const [bodyPart, setBodyPart] = useState('all');
   const [exercises, setExercises] = useState([]);
 
+
+  console.log(bodyPart);
+
   return (
     <Box>
       <HeroBanner/>
@@ -17,9 +20,9 @@ const Home = () => {
         setBodyPart={setBodyPart}
       />
       <Exercises
+        exercises={exercises}
         setExercises={setExercises}
         bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
       />
     </Box>
   )
